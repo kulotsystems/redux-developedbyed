c1d5bcfcb392aeb8feb37cbf10406e779b4fe338
kulotsystems <kulotsystems@gmail.com>
2022-12-08 16:26:05 +0800

    Separate reducers.

--- src/main.jsx
+++ src/main.jsx
@@ -3,46 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { createStore } from 'redux';
+import allReducers from './reducers/index.js';
 
-
-// ACTION INCREMENT
-const increment = () => {
-    return {
-        type: 'INCREMENT'
-    }
-}
-
-// ACTION DECREMENT
-const decrement = () => {
-    return {
-        type: 'DECREMENT'
-    }
-}
-
-// REDUCER
-const counter = (state = 0, action) => {
-    switch (action.type) {
-        case 'INCREMENT':
-            return state + 1;
-        case 'DECREMENT':
-            return state - 1;
-    }
-}
-
-// STORE -> Globalized State
-let store = createStore(counter);
-
-
-// SUBSCRIBE
-store.subscribe(() => {
-    console.log(store.getState());
-});
-
-// DISPATCH
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(decrement());
-
+const store = createStore(allReducers);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
