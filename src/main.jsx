718dff375e0e50073c33992c580b2968ba602deb
kulotsystems <kulotsystems@gmail.com>
2022-12-08 16:40:15 +0800

    Hook store into the app.

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import App from './App';
 import './index.css';
 import { createStore } from 'redux';
 import allReducers from './reducers/index.js';
+import { Provider } from 'react-redux';
 
-const store = createStore(allReducers);
+const store = createStore(
+    allReducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
+    <Provider store={store}>
         <App/>
-    </React.StrictMode>
-)
+    </Provider>
+);
